e10862a6a9dd4ab743929034922a27de719a6829
David Goodman <goodman.david.h@gmail.com>
2014-03-14 10:57:47 -0700

    hardcoded cities will scrape later

--- thrones_faker.rb
+++ thrones_faker.rb
@@ -2,14 +2,14 @@ class DavidFaker
 	def self.name
 		["Joffrey Lannister", "Cersei Lannister", "Tywin Lannister", "Tyrion Lannister",
 		"Brienne Tarth", "Bran Stark", "Arya Stark", "Rob Stark", "Theon Greyjoy", "Peter Baelish", "Stannis Baratheon",
-		"Daenerys Targaryen", "Jorah Mormont" ].sample
+		"Daenerys Targaryen", "Jorah Mormont", "Mance Raydar" ].sample
 	end
 
-	def self.town
-		["Winterfell"]
+	def self.city
+		["Winterfell", "Braavos", "Meereen", "Volantis", "King's Landing", "Asshai",
+			"Qarth", "Astapor", "Vaes Dothrak", "Yunkai", "Pentos", "Valyria", "Lorath", "Sunspear", "Lys",
+			"Myr", "Qohor", "Tyrosh", "White Harbor", "Lannisport", "Old Ghis", "Norvos", "Elyria",
+			"New Ghis", "Tolos", "Mantarys"].sample
 	end
-	
+
 end
-
-
-p DavidFaker.name
