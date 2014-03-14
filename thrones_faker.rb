d2982e2090fa18d534c836cb1e80596715c7d761
David Goodman <goodman.david.h@gmail.com>
2014-03-14 10:33:41 -0700

    added a few names, will return later

--- thrones_faker.rb
+++ thrones_faker.rb
@@ -1,12 +1,14 @@
 class DavidFaker
 	def self.name
 		["Joffrey Lannister", "Cersei Lannister", "Tywin Lannister", "Tyrion Lannister",
-		"Brienne of Tarth", "Bran Stark", "Arya Stark", "Rob Stark" ].sample
+		"Brienne Tarth", "Bran Stark", "Arya Stark", "Rob Stark", "Theon Greyjoy", "Peter Baelish", "Stannis Baratheon",
+		"Daenerys Targaryen", "Jorah Mormont" ].sample
 	end
 
 	def self.town
 		["Winterfell"]
 	end
+	
 end
 
 
